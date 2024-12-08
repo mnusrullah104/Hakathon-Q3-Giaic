bea5cf904baa7d50b91ffde16d998facc0b59614
SaadAslam58 <saadxpert58@gmail.com>
2024-12-09 01:32:39 +0500

    fourth commit

--- src/projectCard/Card.tsx
+++ src/projectCard/Card.tsx
@@ -37,7 +37,7 @@ export const projectData = [
       id: 5,
      title: "Nike Fast",
       description: "Women's Mid-Rise 7/8 Running Leggings with Pockets ",
-      image: "/image-3.svg",
+      image: "/error5.svg",
       price:"PKR 3 795"
     },
 
@@ -46,14 +46,14 @@ export const projectData = [
       id: 6,
        title: "Nike Dri-FIT ADV Division",
       description: "Women's Long-Sleeve Running Top",
-      image: "/image-2.svg",
+      image: "/error6.svg",
       price:"PKR 5 295"
     },
     {
       id: 7,
       title: "Nike Dri-FIT Challenger",
       description: "Men's 18cm (approx.) 2-in-1 Versatile Shorts",
-      image: "/image-1.svg",
+      image: "/error7.svg",
       price:"PKR 2 495"
     },
    
